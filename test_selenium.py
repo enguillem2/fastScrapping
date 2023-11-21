0d374832359e9566dfb6c7bdc57eff6d9dbaa505
Tu Nombre <you@example.com>
2023-11-21 13:09:40 +0100

    open headless browser

--- test_selenium.py
+++ test_selenium.py
@@ -1,4 +1,21 @@
 # selenium 3
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 
+
+
+def iniciar_chrome():
+    ruta = ChromeDriverManager().install()
+    s=Service(ruta)
+    op = webdriver.ChromeOptions()
+    op.add_argument('--headless')
+    
+
+    driver  = webdriver.Chrome(service=s,options=op)
+    url="https://www.pccomponentes.com/lenovo-ideapad-gaming-3-gen-6-intel-core-i5-11320h-16gb-512gb-ssd-gtx-1650-156"
+    driver.get(url)
+
+if __name__=="__main__":
+    iniciar_chrome()
