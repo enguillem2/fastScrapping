649e87429e39aaacdea65392b289d173e9bc7ebf
Tu Nombre <you@example.com>
2023-11-22 08:46:03 +0100

    mornig tests

--- test_selenium.py
+++ test_selenium.py
@@ -49,12 +49,14 @@ def iniciar_chrome():
 if __name__=="__main__":
     driver=iniciar_chrome()
     url="https://www.pccomponentes.com/lenovo-ideapad-gaming-3-gen-6-intel-core-i5-11320h-16gb-512gb-ssd-gtx-1650-156"
+    url="https://www.pccomponentes.com/alurin-go-start-intel-celeron-n4020-8gb-256gb-ssd-156"
     driver.get(url)
-
-    nombre_producto = driver.find_element(By.ID,"pdp-title")
-    original_price = driver.find_element(By.ID,"pdp-price-original-sticky")
-    print(f"NOMBRE PRODUCTO: {nombre_producto.tag_name}")
-    print(f"original price: {original_price.tag_name}")
+    nombre_producto = driver.find_element(By.ID,"pdp-title").text 
+    original_price = driver.find_elements(By.ID,"pdp-price-original-sticky")
+    price_sell = driver.find_element(By.ID,"pdp-price-current-integer-sticky").text
+    print(f"NOMBRE PRODUCTO: {nombre_producto}")
+    print(f"original price: {original_price}")
+    print(f"price sell: {price_sell}")
 
     input("PULSA ENTER")
     driver.quit()
